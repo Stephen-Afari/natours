75ec474907e58fc5f0b2e1519cafab980c2b2fa8
Stephen-Afari <demorganafari@yahoo.com>
2022-09-21 16:39:07 +0000

    bug fix

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ const server = app.listen(port, () => {
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION!  Shutting down...');
   console.log(err.name, err.message);
-  //console.log(err);
-  server.close(() => {
-    process.exit(1);
-  });
+  //console.log(err); deliberately --change later
+  // server.close(() => {
+  //   process.exit(1);
+  // });
 });
 
 process.on('SIGTERM', () => {
