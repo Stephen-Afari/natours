cbd2316404bc99bde8fbfd0851c245c018b8c71b
Stephen-Afari <demorganafari@yahoo.com>
2022-09-23 20:04:25 +0000

    stripe fix2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -8,58 +8,65 @@ const User = require('../models/userModel');
 const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-  // 1) Get the currently booked tour
+  //1) Get the Currently booked tour
+  const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
   const tour = await Tour.findById(req.params.tourId);
-
-  // 2) Create checkout session
+  //2) create the checkout session
   const session = await stripe.checkout.sessions.create({
+    //Added this
+    // expand: ['line-items'],
     payment_method_types: ['card'],
-    // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
+    // success_url: `${req.protocol}://${req.get('host')}/?tour=${
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
-    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
     line_items: [
       {
-        name: `${tour.name} Tour`,
-        description: tour.summary,
-        images: [
-          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
-        ],
-        amount: tour.price * 100,
-        currency: 'usd',
+        // name: `${tour.name} Tour`,
+        // description: tour.summary,
+        // images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        // amount: tour.price * 100,
+        // currency: 'usd',
+        // quantity: 1,
         quantity: 1,
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
+          },
+        },
       },
     ],
-  });
 
-  // 3) Create session as response
+    mode: 'payment',
+  });
+  //3) create session as response
   res.status(200).json({
     status: 'success',
     session,
   });
 });
-
 // exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-//   // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
+//   //This is only temporary , becasue it's unsecured, everyyonecan make bookings without paying
 //   const { tour, user, price } = req.query;
-
 //   if (!tour && !user && !price) return next();
 //   await Booking.create({ tour, user, price });
-
 //   res.redirect(req.originalUrl.split('?')[0]);
 // });
-
-// /webhook-checkout
-// checkout.session.completed
-
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email }))._id;
-  const price = session.amount_total / 100;
-
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = session.amount_subtotal / 100;
   await Booking.create({ tour, user, price });
 };
 //Here
