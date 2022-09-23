e1715072385ec8a76037dc118e3a50cf89dbd6d6
Stephen-Afari <demorganafari@yahoo.com>
2022-09-23 21:06:34 +0000

    test3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -68,7 +68,8 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_details.email }))
     .id;
-  const price = session.amount_total / 100;
+  const price = 100;
+  // const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
   //console.log(tour, user, price);
 };
